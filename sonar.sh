a7282564f421610bfeb739d88c6cf0dc4bdfbb7b
Bheem Medi <eeswar887@gmail.com>
2025-11-25 12:48:33 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -8,7 +8,7 @@ useradd sonar
 chown sonar:sonar sonarqube-8.9.6.50800 -R
 chmod 777 sonarqube-8.9.6.50800 -R
 su - sonar
-docker run --name sonarqube-custom -p 9000:9000 sonarqube:10.6-community
+docker run -d --name sonarqube-custom -p 9000:9000 sonarqube:10.6-community
 #run this on server manually
 #sh /opt/sonarqube-8.9.6.50800/bin/linux/sonar.sh start
 #echo "user=admin & password=admin"
